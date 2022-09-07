8103721395e46ab7dfbe038125e89e85f3cbf8bd
Akshay Kachar <22361688+akshaykachar@users.noreply.github.com>
2022-09-07 14:54:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-
 import requests
 #import snowflake.connector
 
@@ -12,12 +11,12 @@ import requests
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
+streamlit.text(fruityvice_response)
 
 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 
 streamlit.title("Akshay Kachar")
