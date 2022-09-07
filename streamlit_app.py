ba1769c6f505a1423322352c1573dec57b8dcf4a
Akshay Kachar <22361688+akshaykachar@users.noreply.github.com>
2022-09-07 14:46:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 #import requests
 #import snowflake.connector
 
@@ -26,8 +28,9 @@ streamlit.text('🥭Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥭Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothiee 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
